85576689793b1d5106b9dc44e61e1d3046b1f8e0
bMil21 <bmil_21@yahoo.com>
2014-03-09 22:54:40 -0400

    Add JS concatenation and minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,20 +16,52 @@ module.exports = function(grunt) {
 				}
 			}
 		}, 
+		// Concat
+		concat: {
+			options: {
+				separator: '\n\n;'
+			},
+			dist: {
+				src: ['js/plugins/*.js'],
+				dest: 'js/scripts.js'
+			}
+		}, 
+		// Uglify
+		uglify: {
+			options: {
+				mangle: false,
+				preserveComments: 'some'
+			},
+			my_target: {
+				files: {
+					'js/scripts.js': ['js/scripts.js']
+				}
+			}
+		}, 
 		// Watch
 		watch: {
 			stylus: {
 				files: ['sass/*.scss'], 
 				tasks: ['sass']
+			}, 
+			scripts: {
+				files: ['js/plugins/*.js'], 
+				tasks: 'concat'
+			}, 
+			uglify: {
+				files: ['js/scripts.js'],
+				tasks: 'uglify'
 			}
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
-	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.registerTask('default', ['sass', 'concat', 'uglify', 'watch']);
 
 };
