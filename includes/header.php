db254bf0852b9eb254932c8ccab387887b9857e7
Brandon Miller <brandonm@aycmedia.com>
2014-07-22 09:31:11 -0400

    Update Conditional IE tags

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,10 @@
 // Header
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
-<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if IE 6 ]><html class="no-js ie ie6 lt-ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 9 ]><html class="no-js ie ie9 lt-ie10"><![endif]-->
 <!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
